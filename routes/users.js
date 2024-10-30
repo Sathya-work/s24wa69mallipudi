6dbc34313e4eec137a85fc36bfa3f0763c0a7eb2
Sathya-work <sathyanarayana.work@gmail.com>
2024-10-30 12:06:04 -0500

    Update users.js
    Updated user.js with value and total

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var value = 0;
+var total = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  value += 2;
+  total += value;
+  res.send(`Total is: ${total}`);
 });
 
 module.exports = router;
